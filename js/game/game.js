06bf38ae25a0aa07b55414da635325a885cb4a9a
Suhail Abood <suhail.abood@gmail.com>
2014-11-05 11:47:56 +0000

    organized

---
+++ js/game/game.js
@@ -0,0 +1,13 @@
+com = com || {};
+com.suh = com.suh || {};
+com.suh.games = com.suh.games || {};
+com.suh.games.NinjaSanta = function(){
+
+};
+
+com.suh.games.NinjaSanta.prototype = {
+	init:function(){
+		
+	},
+	
+};
